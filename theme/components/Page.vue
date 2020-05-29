da7df3962d743993089b7fbbfb330bea13d68e88
artiely <1119696785@qq.com>
2020-05-29 17:36:29 +0800

    样式更新

--- theme/components/Page.vue
+++ theme/components/Page.vue
@@ -2,7 +2,7 @@
   <main class="page">
     <slot name="top" />
 
-    <Content class="theme-antdocs-content" />
+    <Content class="theme-antdocs-content"/>
     <div style="margin-top:-100px;padding-bottom:20px">
     <PageEdit />
     <PageNav v-bind="{ sidebarItems }" />
